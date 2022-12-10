12dac7e2d87eadf3b32c0af796e6df3088c4be2b
Luca Paaschen <lucapaaschen42@gmail.com>
2022-12-10 21:21:40 +0100

    commit

--- tipper.py
+++ tipper.py
@@ -1,7 +1,23 @@
+import os
 from flask import Flask
+import db
+
+app = Flask(__name__)
+
+app.config.from_mapping(
+    SECRET_KEY='secret_key_just_for_dev_environment',
+    DATABASE=os.path.join(app.instance_path, 'todos.sqlite')
+)
+app.cli.add_command(db.init)
+app.teardown_appcontext(db.close)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hallo Bundesliga!' 
+    return 'Hallo Bundesliga!'
+
+@app.route('/insert/sample')
+def insert_sample():
+    db.insert_sample()
+    return 'Database flushed and populated with some sample data.'
