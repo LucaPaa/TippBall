a347d0d7096f89364fd95e493d55bc21a779b2fc
Luca Paaschen <lucapaaschen42@gmail.com>
2022-12-21 03:44:43 +0100

    commit

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 import db
 
 app = Flask(__name__)
 
-app.config.from_mapping(
-    SECRET_KEY='secret_key_just_for_dev_environment',
-    DATABASE=os.path.join(app.instance_path, 'todos.sqlite')
-)
-app.cli.add_command(db.init)
-app.teardown_appcontext(db.close)
-
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('bundesliga.html')
 
-@app.route('/wow')
-def wow():
-    return 'Wow, this is my second working URL!'
-
-@app.route('/insert/sample')
-def insert_sample():
-    db.insert_sample()
-    return 'Database flushed and populated with some sample data.'
+if __name__ == '__main__':
+    app.run(debug = True)
