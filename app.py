b3263f5d68cd8ac8c2b354ee56b35cf4677f3b5b
Luca Paaschen <lucapaaschen42@gmail.com>
2023-01-12 19:17:28 +0100

    commit

--- app.py
+++ app.py
@@ -1,16 +1,79 @@
 import os
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-import db
+from flask_migrate import Migrate, migrate
+# import db
 
 app = Flask(__name__)
 
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+app.app_context().push()
+ 
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+# Settings for migrations
+migrate = Migrate(app, db)
+
+# Models
+class Profile(db.Model):
+    # Id : Field which stores unique id for every row in
+    # database table.
+    # first_name: Used to store the first name if the user
+    # last_name: Used to store last name of the user
+    # Age: Used to store the age of the user
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+ 
+    # repr method represents how one object of this datatable
+    # will look like
+    def __repr__(self):
+        return f"Name : {self.first_name}, Age: {self.age}"
+ 
+
+@app.route('/add_data')
+def add_data():
+    return render_template('add_profile.html')
+
+# function to add profiles
+@app.route('/add', methods=["POST"])
+def profile():
+     
+    # In this function we will input data from the
+    # form page and store it in our database.
+    # Remember that inside the get the name should
+    # exactly be the same as that in the html
+    # input fields
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    age = request.form.get("age")
+ 
+    # create an object of the Profile class of models
+    # and store data as a row in our datatable
+    if first_name != '' and last_name != '' and age is not None:
+        p = Profile(first_name=first_name, last_name=last_name, age=age)
+        db.session.add(p)
+        db.session.commit()
+        return redirect('/profile')
+    else:
+        return redirect('/profile')
 
 @app.route('/')
 def index():
     return render_template('bundesliga.html')
+
+@app.route('/profile')
+def profil():
+    profiles = Profile.query.all()
+    return render_template('profile.html', profiles=profiles)
 
 @app.route('/tipps')
 def tipps():
@@ -36,7 +99,16 @@ def register():
     if request.method == 'POST' and form.validate():
 
             return render_template('register.html', form=form)  
-    return render_template('register.html', form=form)  
+    return render_template('register.html', form=form) 
+
+@app.route('/delete/<int:id>')
+def erase(id):
+    # Deletes the data on the basis of unique id and
+    # redirects to home page
+    data = Profile.query.get(id)
+    db.session.delete(data)
+    db.session.commit()
+    return redirect('/profile')
 
 if __name__ == '__main__':
     app.run(debug = True)
