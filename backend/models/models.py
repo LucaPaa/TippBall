07f48bd4fa3e396be4f95a58fe11319ae5ddcb66
Luca Paaschen <lucapaaschen42@gmail.com>
2023-01-15 21:56:04 +0100

    commit

--- backend/models/models.py
+++ backend/models/models.py
@@ -16,3 +16,23 @@ class Profile(Base):
     # will look like
     def __repr__(self):
         return f"Name : {self.first_name}, Age: {self.age}"
+
+class Klubs(Base):
+    __tablename__ = "klubs"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), unique=True, nullable=False)
+    name_short = Column(String(15), unique=True, nullable=False)
+    image = Column(String(80), unique=True, nullable=False)
+    points = Column(Integer, unique=False, nullable=False)
+    diff = Column(Integer, unique=False, nullable=False)
+    goals_for = Column(Integer, unique=False, nullable=False)
+    goals_against = Column(Integer, unique=False, nullable=False)
+    matches = Column(Integer, unique=False, nullable=False)
+    wins = Column(Integer, unique=False, nullable=False)
+    draws = Column(Integer, unique=False, nullable=False)
+    losses = Column(Integer, unique=False, nullable=False)
+
+    
+
+
