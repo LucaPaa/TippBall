f5a71ae21662bf7a1ebbcbfda64bea8eef4d0e2a
Luca Paaschen <lucapaaschen42@gmail.com>
2023-01-24 20:18:53 +0100

    login feature

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,22 +1,17 @@
 from database.database import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base, relationship
+from flask_login import UserMixin
 
 
-class Profile(Base):
-    __tablename__ = "profile"
-
+class Register(Base, UserMixin):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(20), unique=False, nullable=False)
-    last_name = Column(String(20), unique=False, nullable=False)
-    age = Column(Integer, nullable=False)
-    mail = Column(String(40), nullable=False, unique=True)
-    password = Column(String(20), nullable=False, unique=False)
+    username = Column(String(20), unique=True, nullable=False)
+    password = Column(String(80), nullable=False, unique=False)
+    points = Column(Integer, unique=False, default=0)
+    
 
-    # repr method represents how one object of this datatable
-    # will look like
-    def __repr__(self):
-        return f"Name : {self.first_name}, Age: {self.age}"
 
 class Klubs(Base):
     __tablename__ = "klubs"
